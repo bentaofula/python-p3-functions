ffbb9f691796e9d5757224e54e27b657365b6900
Oscar Okumu <ookumu@songa.app>
2023-05-18 11:59:28 +0300

    updated

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
-
+    return num1 + num2
 def halve(number):
-    pass
+    return number / 2
+
+def test_halve_int():
+    result = halve(100)
+    assert result == 50
+
+test_halve_int()
+def halve(number):
+    return number / 2
+
+def test_halve_float():
+    result = halve(99.0)
+    assert result == 49.5
+
+test_halve_float()
